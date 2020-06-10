21aa73ee693a4ba65f5725d81d88dbef05499ba1
Izzy Swart <zenerboson@gmail.com>
2020-06-09 17:44:29 -0700

    demonstrate protocol export

--- examples/run-test-vessel/src/lib.rs
+++ examples/run-test-vessel/src/lib.rs
@@ -1,61 +1,31 @@
-use core::{
-    convert::{Infallible, TryFrom},
-    pin::Pin,
-    task::{Context, Poll},
-};
-use core_futures_io::AsyncWrite;
-use std::{fs::read, string::FromUtf8Error};
+use core::convert::Infallible;
+use futures::task::{FutureObj, LocalFutureObj, LocalSpawn, Spawn, SpawnError};
+use protocol_mve_transport::ProtocolMveTransport;
+use std::panic;
 use vessels::{
     register,
     resource::ResourceManagerExt,
-    runtime::{Runtime, Wasm},
+    runtime::{CoalesceFramed, ModuleResource, Wasm},
     with_core, Convert, Core, MemoryStore, Ring, Sha256, SimpleResourceManager,
 };
-use wasm_bindgen_futures::spawn_local;
 use vessels_web::WebRuntime;
 use wasm_bindgen::prelude::wasm_bindgen;
-use std::panic;
+use wasm_bindgen_futures::spawn_local;
 use web_sys::console::log_1;
 
-#[derive(Debug, Clone)]
-pub struct Tester(String);
+pub struct SpawnShim;
 
-impl TryFrom<Vec<u8>> for Tester {
-    type Error = FromUtf8Error;
-
-    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
-        String::from_utf8(value).map(Tester)
+impl LocalSpawn for SpawnShim {
+    fn spawn_local_obj(&self, future: LocalFutureObj<'static, ()>) -> Result<(), SpawnError> {
+        spawn_local(future);
+        Ok(())
     }
 }
 
-impl From<Tester> for Vec<u8> {
-    fn from(tester: Tester) -> Vec<u8> {
-        tester.0.as_bytes().into()
-    }
-}
-
-pub struct TestWriter;
-
-impl AsyncWrite for TestWriter {
-    type WriteError = Infallible;
-    type FlushError = Infallible;
-    type CloseError = Infallible;
-
-    fn poll_write(
-        self: Pin<&mut Self>,
-        _: &mut Context,
-        buf: &[u8],
-    ) -> Poll<Result<usize, Self::WriteError>> {
-        log_1(&format!("got data {:?}", buf).into());
-        Poll::Ready(Ok(buf.len()))
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, _: &mut Context) -> Poll<Result<(), Self::FlushError>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn poll_close(self: Pin<&mut Self>, _: &mut Context) -> Poll<Result<(), Self::CloseError>> {
-        Poll::Ready(Ok(()))
+impl Spawn for SpawnShim {
+    fn spawn_obj(&self, future: FutureObj<'static, ()>) -> Result<(), SpawnError> {
+        spawn_local(future);
+        Ok(())
     }
 }
 
@@ -84,18 +54,20 @@ async fn entry() {
         .await
         .unwrap();
 
-        let resource = store
+        let resource: ModuleResource<String> = store
             .intern::<Ring, _, Convert>(Wasm(
                 include_bytes!("../../../target/wasm32-unknown-unknown/debug/test_vessel.wasm").to_vec(),
             ))
             .await
-            .unwrap();
+            .unwrap().into();
 
         let mut runtime = WebRuntime;
 
-        runtime
-            .instantiate(resource, TestWriter, [10u8, 2u8, 3u8, 50u8].as_ref())
+        let data = runtime
+            .coalesce_framed_local::<_, _, ProtocolMveTransport<_>>(SpawnShim, resource)
             .await
             .unwrap();
+
+        log_1(&data.into());
     }};
 }
--- examples/test-vessel/src/lib.rs
+++ examples/test-vessel/src/lib.rs
@@ -1,13 +1,9 @@
-use core_futures_io::copy;
-use futures::task::SpawnExt;
-use vessels::{export, VesselEntry};
+use protocol_mve_transport::ProtocolMveTransport;
+use vessels::{runtime::RawAdapter, vessel};
 
-export! {
-    VesselEntry { spawner, mut reader, mut writer } => {
-        spawner.spawn(async move {
-            copy(&mut reader, &mut writer)
-                .await
-                .unwrap_or_else(|_| panic!());
-        }).unwrap();
+vessel! {
+    using RawAdapter<_, _, _, _, ProtocolMveTransport<_>>;
+    spawner => {
+        "hello there".to_owned()
     }
 }
